3c5d2caa810ec68deab556f557e45a8976a12c09
xuzhen97 <xuzhen97@outlook.com>
2026-06-03 15:35:25 +0800

    refactor(client): remove unreachable legacy task dispatcher and executors

--- apps/client/src/executors/file-service-status.executor.ts
+++
@@ -1,5 +0,0 @@
-import { getFileHttpServerStatus } from '../runtime/file-http-server.js';
-
-export async function executeFileServiceStatus(): Promise<unknown> {
-  return getFileHttpServerStatus();
-}
--- apps/client/src/executors/file-service-stop.executor.ts
+++
@@ -1,6 +0,0 @@
-import { stopFileHttpServer } from '../runtime/file-http-server.js';
-
-export async function executeFileServiceStop(): Promise<unknown> {
-  await stopFileHttpServer();
-  return { stopped: true };
-}
